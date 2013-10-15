3383aa1d871ed98594c1c382f31837edd2bd4524
Andrew Evans <andrewevans94@gmail.com>
2013-10-15 19:06:09 -0400

    Updated rubixcube class

--- src/RubixCube.java
+++ src/RubixCube.java
@@ -1,4 +1,26 @@
 
+import java.io.*;
+import java.util.*;
+
 public class RubixCube {
+
+	private final byte R = 0;
+	private final byte G = 1;
+	private final byte Y = 2;
+	private final byte B = 3;
+	private final byte O = 4;
+	private final byte W = 5;
+
+	private byte[][] cube;
+
+	public void loadFromFile(String filename) {
+		File f = new File(filename);
+		Scanner s = new Scanner(f);
+
+		while(s.hasNext()) {
+			
+		}
+	}
+
 	
 }
