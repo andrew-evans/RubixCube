126a0810f125076381c1c74798ec3dc49b9ae27c
Andrew Evans <andrewevans94@gmail.com>
2013-10-18 21:03:35 -0400

    added new text files and started working on tablegenerator

---
+++ src/TableGenerator.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.*;
+import RubixCube;
+
+public class TableGenerator {
+
+	private RubixCube goal;
+
+	public void makeCubieTable() {
+		goal.loadFromFile("goal-cubies.txt");
+	}
+
+}
