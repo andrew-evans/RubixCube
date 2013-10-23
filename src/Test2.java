df911465264694220879b6613160b1ba58270a5a
deceptivecadence <michaelmucci64@gmail.com>
2013-10-23 14:24:01 -0400

    adfasdfasdf

--- src/Test2.java
+++ src/Test2.java
@@ -8,15 +8,15 @@ public class Test2 {
 		//TableGenerator tg = new TableGenerator();
 		//tg.main();
 		
-		Search2 s = new Search2("rubbix.txt");
+		Search s = new Search("rubbix.txt");
 		//RubixCube goal = new RubixCube("goal.txt");
 		/*System.out.println(""+(int)s.table1[goal.getIndexCorner()]);
 		System.out.println(""+(int)s.table2[goal.getIndexEdge1()]);
 		System.out.println(""+(int)s.table3[goal.getIndexEdge2()]);*/
 		long start = new Date().getTime();
 		System.out.println("Started Search");
-		//System.out.println(s.IDA());
-		System.out.println(s.ASearch());
+		System.out.println(s.IDA());
+		//System.out.println(s.ASearch());
 		long end = new Date().getTime() - start;
 		System.out.println("----------Finish!-------------   "+ end/60000 + " min to complete.");
 		//System.out.println(s.frontier.toString());
