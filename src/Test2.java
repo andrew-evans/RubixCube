4ab8315f735009c5f729830f5ab87dc609d2e934
andrew-evans <andrewevans94@gmail.com>
2013-10-22 20:12:35 -0400

    added a new a* search method. probs won't work yo

--- src/Test2.java
+++ src/Test2.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class Test2 {
 
 	public static void main(String[] args) throws IOException {
-		Search s = new Search("rubbix.txt");
+		Search s = new Search2("rubbix.txt");
 		long start = new Date().getTime();
 		System.out.println("Started Search");
-		System.out.println(s.IDA());
+		System.out.println(s.ASearch());
 		long end = new Date().getTime() - start;
 		System.out.println("----------Finish!-------------   "+ end/60000 + " min to complete.");
 		//System.out.println(s.frontier.toString());
