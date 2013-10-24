b1446cccabcbcd41dd4e888ec566189bf9363e9d
deceptivecadence <michaelmucci64@gmail.com>
2013-10-24 17:30:47 -0400

    wah-hey!

--- src/Search.java
+++ src/Search.java
@@ -2,7 +2,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
 
@@ -11,27 +10,15 @@ class Search {
 	protected byte[] table1 = new byte[88179840];
 	protected byte[] table2 = new byte[42577920];
 	protected byte[] table3 = new byte[42577920];
-	/*private final byte R = 0;
-	private final byte G = 1;
-	private final byte Y = 2;
-	private final byte B = 3;
-	private final byte O = 4;
-	private final byte W = 5;*/
-	private String history = "Moves: ";
+
 	public PriorityQueue<RubixCube> frontier;
-	//private byte[] faceArray = {R,G,Y,B,O,W};
 	final RubixCube goalCube;
 	final RubixCube inputCube;
 	private boolean success = false;
 	public Search(String inputCubeFile) throws IOException {
 		
-		//TableGenerator test = new TableGenerator();
-		
-		//test.main();
-		
 		this.goalCube = new RubixCube("goal.txt");
 		this.inputCube = new RubixCube(inputCubeFile);
-		//this.inputCube.rotateCube(Y).rotateCube(Y).rotateCube(Y).rotateCube(B).rotateCube(B).rotateCube(B);
 		this.frontier = new PriorityQueue<RubixCube>(11,new RubixCubeComparator());
 		
 		FileInputStream fis1 = new FileInputStream("heuristic-tables/cornercubie-table.txt");
@@ -69,60 +56,28 @@ class Search {
 		}
 		
 		fis3.close();
-		
-		
-		//File f = new File("heuristic-tables/cubie-table.txt");
-		//Scanner s = new Scanner(f);
-		
-		
-		//System.out.println(f.exists());
-		
-		/*
-		 * RubixCube test = new RubixCube("rubbix.txt");
-		RubixCube test2 = new RubixCube("rubbix.txt");
-		
-		byte R = 0;
-		byte G = 1;
-		byte Y = 2;
-		byte B = 3;
-		byte O = 4;
-		byte W = 5;
-		
-		test.rotateCube(R);
-		test.rotateCube(G);
-		test.rotateCube(Y);
-		test.rotateCube(B);
-		test.rotateCube(O);
-		test.rotateCube(W);
-		
-		//System.out.println(test.toString());
-		
-		//aStar(test2,R);
-		*/
+
 	}
 	
-	public ArrayList<Integer> IDA() throws FileNotFoundException{
-		//boolean solutionFound = false;
+	public String IDA() throws FileNotFoundException{
+
 		int bound = 1;
 		ArrayList<Integer> path = new ArrayList<Integer>();
-		//System.out.println(heuristic(this.goalCube));
+
 		while (!success){
 			this.frontier.clear();
-			//System.out.println("Beginning of While: "+rotationsSoFar);
-			//this.history = "Moves: ";
+
 			int rotationsSoFar = 0;
 			int costSoFar = 0;
-			//RubixCube solution = null;
+
 			path = new ArrayList<Integer>(); 
 			this.frontier.add(this.inputCube);
-			path = aStar(this.frontier.remove(), costSoFar, rotationsSoFar, bound, new ArrayList<Integer>());//, this.hist);
-			/*if(Arrays.deepEquals(solution.getCube(), this.goalCube.getCube())){
-				solutionFound = true;
-			};*/
-			 bound += 1;
+			path = aStar(this.frontier.remove(), costSoFar, rotationsSoFar, bound, new ArrayList<Integer>());
+
+			bound += 1;
 		}
-		return path;
-		//return this.history;
+		
+		return convertPath(path);
 	}
 	
 	public ArrayList<Integer> aStar(RubixCube cube, int costSoFar, int rotationsSoFar, int bound, ArrayList<Integer> hist){
@@ -131,28 +86,8 @@ class Search {
 			this.success = true;
 			return hist;
 		}
-			
-		/*if(rotationsSoFar % 1000 == 0){
-			System.out.println("A* working..."+bound);
-		}*/
-		//System.out.println("1st best node with bound: "+bound+" and rotation: "+rotationsSoFar+"\n"+cube);
+		
 		if(rotationsSoFar < bound){
-			
-			//byte[] history = hist;
-			//byte fn = costSoFar + heuristic(cube);
-			/*RubixCube node1 = RubixCube.newInstance(cube.getCube());
-			RubixCube node2 = RubixCube.newInstance(cube.getCube());
-			RubixCube node3 = RubixCube.newInstance(cube.getCube());
-			RubixCube node4 = RubixCube.newInstance(cube.getCube());
-			RubixCube node5 = RubixCube.newInstance(cube.getCube());
-			RubixCube node6 = RubixCube.newInstance(cube.getCube());
-			RubixCube[] cubeArray = {node1,node2,node3,node4,node5,node6};
-			
-			int[] fnArray = {-1,-1,-1,-1,-1,-1};*/
-			/*node1.rotateCube(R);
-			System.out.println(node1.toString());
-			node2.rotateCube(R);
-			System.out.println(node2.toString());*/
 			
 			for(byte i=0; i<6; i++){
 				RubixCube node = RubixCube.newInstance(cube.getCube());
@@ -160,12 +95,14 @@ class Search {
 				node.setfunctionVal(costSoFar + heuristic(node));
 				frontier.add(node);
 			}
+			
 			for(byte i=0; i<6; i++){
 				RubixCube node = RubixCube.newInstance(cube.getCube());
 				node.rotateCube(i).rotateCube(i);
 				node.setfunctionVal(costSoFar + heuristic(node));
 				frontier.add(node);
 			}
+			
 			for(byte i=0; i<6; i++){
 				RubixCube node = RubixCube.newInstance(cube.getCube());
 				node.rotateCube(i).rotateCube(i).rotateCube(i);
@@ -173,65 +110,40 @@ class Search {
 				frontier.add(node);
 			}
 			
-			//System.out.println(Arrays.toString(fnArray));
-			//int smallestFn = 9999;
-			//byte indexOfBest = -1;
-			/*			
-			for(byte i=0; i<fnArray.length; i++){
-				if (fnArray[i]<=smallestFn){
-					smallestFn = fnArray[i];
-					indexOfBest = i;
-				}
-			}*/
-			//System.out.println(indexOfBest);
-			//history[indexOfBest] += 1;
-			//System.out.println("---------PQ---------");
-			//System.out.println(this.frontier.toString());
-			//System.out.println("---------PQ---------");
-			//System.out.println("2nd best node with bound: "+bound+" and rotation: "+rotationsSoFar+"\n"+this.frontier.element().toString());
-			//System.out.println("Heuristic: "+this.frontier.element().getHeuristic());
-			//System.out.println("Passed recursively from array @ index "+indexOfBest+"(lowest fn=" +fnArray[indexOfBest] +" ):\n"+cubeArray[indexOfBest].toString());
-			//System.out.println("Passed recursively from PQ(lowest fn="+this.frontier.element().getfunctionVal()+" val):\n"+this.frontier.element().toString());
 			rotationsSoFar += 1;
 			costSoFar += 1;
-			/*for(byte i=0; i<cubeArray.length; i++){
-				if(this.frontier.element().getCube() == cubeArray[i].getCube()){
-					indexOfBest = i;
-				}
-			}*/
-			//this.history += this.frontier.element().lastMove + ", ";
-			//hist.add((int) this.frontier.element().lastMoveList);
+
 			hist.addAll(this.frontier.element().lastMoveList);
 			return aStar(this.frontier.remove(), costSoFar, rotationsSoFar, bound, hist);
 		}
 		return hist;
-			
-			/*if (Arrays.deepEquals(this.frontier.element().getCube(), this.goalCube.getCube()) ){
-				return this.frontier.remove();
-			}
-			else{
-				// - costSoFar;
-				//return aStar(this.frontier.remove(), costSoFar, rotationsSoFar, bound, hist);//, this.hist);
-				//if(Arrays.deepEquals(solution.getCube(), this.goalCube.getCube())){
-				//	return hist;
-				//}
-			}
-		}
-		//System.out.println("bound reached in IDA"+bound);
-		//return cube;
-		
-		/*node1.rotateCube(R);
-		node2.rotateCube(R);
-		System.out.println(node1.toString());
-		System.out.println(node2.toString());
-		*/
+
 	}
 	
 	public int heuristic(RubixCube cube){
-		//System.out.println(""+(int)this.table1[cube.getIndexCorner()]);
-		//System.out.println(""+(int)this.table2[cube.getIndexEdge1()]);
-		//System.out.println(""+(int)this.table3[cube.getIndexEdge2()]);
 		return Math.max(Math.max((int)this.table1[cube.getIndexCorner()],(int)this.table2[cube.getIndexEdge1()]),(int)this.table3[cube.getIndexEdge2()]);
+	}
+	public String convertPath(ArrayList<Integer> oldPath){
+		int previous = -1;
+		int[] faceCounter = {0,0,0,0,0,0};
+		String[] faces = {"R","G","Y","B","O","W"};
+		String newPath = "";
+		for (int face : oldPath){
+			if(previous==face){
+				faceCounter[face] += 1; 
+			}else{
+				faceCounter[face] = 1;
+			}
+			previous = face;
+		}
+		previous = -1;
+		for (int face : oldPath){
+			if(previous != face){
+				newPath = newPath + faces[face] + faceCounter[face] + " ";
+				previous = face;
+			}
+		}
+		return newPath;
 	}
 }
 
--- src/Test2.java
+++ src/Test2.java
@@ -5,21 +5,14 @@ import java.util.Date;
 public class Test2 {
 
 	public static void main(String[] args) throws IOException {
-		//TableGenerator tg = new TableGenerator();
-		//tg.main();
+
 		Search s = new Search("rubbix.txt");
-		//RubixCube goal = new RubixCube("goal.txt");
-		/*System.out.println(""+(int)s.table1[goal.getIndexCorner()]);
-		System.out.println(""+(int)s.table2[goal.getIndexEdge1()]);
-		System.out.println(""+(int)s.table3[goal.getIndexEdge2()]);*/
+
 		long start = new Date().getTime();
 		System.out.println("Started Search");
 		System.out.println(s.IDA());
-		//System.out.println(s.ASearch());
 		long end = new Date().getTime() - start;
-		System.out.println("----------Finish!-------------   "+ end/60000 + " min to complete.");
-		//System.out.println(s.frontier.toString());
-		//System.out.println("------------------------------");
+		System.out.println("----------Finish!-------------   "+ end/1000 + " sec to complete.");
 
 	}
 
